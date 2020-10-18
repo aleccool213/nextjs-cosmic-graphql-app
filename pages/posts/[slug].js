ba3b633484c08ee1e16e5d98c77437ad6a6cab9c
Alec Brunelle <alec@alec.coffee>
2020-10-18 11:48:36 -0700

    Remove querying for page data
    - to show as an example

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,22 +1,20 @@
-import { useRouter } from 'next/router'
-import ErrorPage from 'next/error'
-import Container from '@/components/container'
-import PostBody from '@/components/post-body'
-import MoreStories from '@/components/more-stories'
-import Header from '@/components/header'
-import PostHeader from '@/components/post-header'
-import SectionSeparator from '@/components/section-separator'
-import Layout from '@/components/layout'
-import { getAllPostsWithSlug, getPostAndMorePosts } from '@/lib/api'
-import PostTitle from '@/components/post-title'
-import Head from 'next/head'
-import { CMS_NAME } from '@/lib/constants'
-import markdownToHtml from '@/lib/markdownToHtml'
+import { useRouter } from "next/router";
+import ErrorPage from "next/error";
+import Container from "@/components/container";
+import PostBody from "@/components/post-body";
+import MoreStories from "@/components/more-stories";
+import Header from "@/components/header";
+import PostHeader from "@/components/post-header";
+import SectionSeparator from "@/components/section-separator";
+import Layout from "@/components/layout";
+import PostTitle from "@/components/post-title";
+import Head from "next/head";
+import { CMS_NAME } from "@/lib/constants";
 
 export default function Post({ post, morePosts, preview }) {
-  const router = useRouter()
+  const router = useRouter();
   if (!router.isFallback && !post?.slug) {
-    return <ErrorPage statusCode={404} />
+    return <ErrorPage statusCode={404} />;
   }
   return (
     <Layout preview={preview}>
@@ -50,29 +48,5 @@ export default function Post({ post, morePosts, preview }) {
         )}
       </Container>
     </Layout>
-  )
-}
-
-export async function getStaticProps({ params, preview = null }) {
-  const data = await getPostAndMorePosts(params.slug, preview)
-  const content = await markdownToHtml(data.post?.metadata?.content || '')
-
-  return {
-    props: {
-      preview,
-      post: {
-        ...data.post,
-        content,
-      },
-      morePosts: data.morePosts || [],
-    },
-  }
-}
-
-export async function getStaticPaths() {
-  const allPosts = (await getAllPostsWithSlug()) || []
-  return {
-    paths: allPosts.map((post) => `/posts/${post.slug}`),
-    fallback: true,
-  }
+  );
 }
